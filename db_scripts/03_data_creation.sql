bdcd80d247a628af67bee032a4ddfdc19f0c41e1
Jay H.J Kwon <nicekhj@gmail.com>
2014-06-20 08:19:47 +1000

    'db_scripts'

---
+++ db_scripts/03_data_creation.sql
@@ -0,0 +1,39 @@
+insert into propertyinfo(Id,PropertyType,Price,Bond,AuctionDate,AvailableDate,Address,Description,AgentId) values(
+1,0,650000,null, '2014-07-01',null,'2/5 unit Court Road Clayton VIC 3168','Furnished Room in Shared House - Perfect for Mandarin Speaking Student
+On the doorstep of everything that Clayton has to offer is this fantastic shared accommodation house. 
+Available to rent is a single bedroom with bed, desk, chair and wardrobe. Sharing the kitchen, meals area, shower & toilet with only one other housemate there is also a large communal lounge and two toilets for the all of the housemates to share. 
+All of your utilities - gas, electricity, water & internet are included in the rent of $150 per week. What a bargain! Available to inspect today.',1)
+
+insert into propertyinfo(Id,PropertyType,Price,Bond,AuctionDate,AvailableDate,Address,Description,AgentId) values(
+2,1,1800,1850,null,'2014-06-28','1/1411 Centre Road Clayton VIC 3168', 'This 2 bedroom double storey townhouse is situated walking distance to Clayton Train Station, Monash Medical Centre and Clayton Shopping precinct. Comprising large lounge with gas heater, kitchen with gas cook top and meals area. Upstairs offers 2 good sized bedrooms with built in robes, family sized bathroom and 2 toilets. There is a secure rear courtyard and off street private carpark.',1)
+
+insert into propertyinfo(Id,PropertyType,Price,Bond,AuctionDate,AvailableDate,Address,Description,AgentId) values(
+3,0,805000,null,'2014-09-01',null,'3/49 Carinish Road, CLAYTON 3168', 'Furnished Room in Shared House - Perfect for Mandarin Speaking Student
+On the doorstep of everything that Clayton has to offer is this fantastic shared accommodation house. 
+Available to rent is a single bedroom with bed, desk, chair and wardrobe. Sharing the kitchen, meals area, shower & toilet with only one other housemate there is also a large communal lounge and two toilets for the all of the housemates to share. 
+All of your utilities - gas, electricity, water & internet are included in the rent of $150 per week. What a bargain! Available to inspect today.',2)
+
+insert into propertyinfo(Id,PropertyType,Price,Bond,AuctionDate,AvailableDate,Address,Description,AgentId) values(
+4,1,2000,2100,null,'2014-07-12','1/6 McGregor Street, CLAYTON 3168','Recently renovated, this fully furnished property has all the bells and whistles to surely surprise you. There are 7 bedrooms, all with built-in-robes, a bar fridge, a bed, desk and chair as well as TV and phone points (for local calls only). You will also have access to cable and wireless internet. There are three separate toilets as well as two separate showers, the kitchen is generous and comes with stainless steel appliances. There is ducted heating to keep you warm in winter and to top it all off, it is located nice and close to Monash university.',3)
+
+insert into propertyinfo(Id,PropertyType,Price,Bond,AuctionDate,AvailableDate,Address,Description,AgentId) values(
+5,0,750000,null,'2014-07-20',null,'2/35 Sarton Road, CLAYTON 3168','This property also consists of a study area, great modern kitchen with gas cooking, very spacious living area, dining area, bathroom with separate shower and bath tub, separate laundry, ducted heating, evaporative cooling, secure yard and single lock up garage.',3)
+
+insert into agent(Id,Name,Phone,Email) values(1,'Hockstuert','0401 757 088','mike@gmail.com')
+insert into agent(Id,Name,Phone,Email) values(2,'Kingdome','03 9888 7373','james@hotmail.com')
+insert into agent(Id,Name,Phone,Email) values(3,'StockdaleLeggo','0404 755 888','robert@gmail.com')
+
+insert into Photo(Id,Name,PropertyId) values(1,'property_1.jpg',1)
+insert into Photo(Id,Name,PropertyId) values(2,'property_2.jpg',1)
+insert into Photo(Id,Name,PropertyId) values(3,'property_3.jpg',1)
+insert into Photo(Id,Name,PropertyId) values(4,'property_4.jpg',1)
+insert into Photo(Id,Name,PropertyId) values(5,'property_5.jpg',2)
+insert into Photo(Id,Name,PropertyId) values(6,'property_6.jpg',2)
+insert into Photo(Id,Name,PropertyId) values(7,'property_7.jpg',2)
+insert into Photo(Id,Name,PropertyId) values(8,'property_8.jpg',3)
+insert into Photo(Id,Name,PropertyId) values(9,'property_9.jpg',3)
+insert into Photo(Id,Name,PropertyId) values(10,'property_10.jpg',4)
+insert into Photo(Id,Name,PropertyId) values(11,'property_11.jpg',4)
+insert into Photo(Id,Name,PropertyId) values(12,'property_12.jpg',5)
+
+
